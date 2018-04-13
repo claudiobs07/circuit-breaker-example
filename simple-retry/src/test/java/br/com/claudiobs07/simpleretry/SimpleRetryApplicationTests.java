99ae0674e4d91cb104f3f3ad275512f781b7da9a
Claudio Santos <claudiobs07@gmail.com>
2018-04-13 11:19:29 -0300

    add: module simple-retry

---
+++ simple-retry/src/test/java/br/com/claudiobs07/simpleretry/SimpleRetryApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.claudiobs07.simpleretry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleRetryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
