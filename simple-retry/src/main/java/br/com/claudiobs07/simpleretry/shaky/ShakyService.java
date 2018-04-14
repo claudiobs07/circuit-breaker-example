48e07c1e2f0f0baf3b9b1b4155b3095a3bcacb52
Claudio Santos <claudiobs07@gmail.com>
2018-04-14 14:09:16 -0300

    add: improve simple retry

--- simple-retry/src/main/java/br/com/claudiobs07/simpleretry/shaky/ShakyService.java
+++ simple-retry/src/main/java/br/com/claudiobs07/simpleretry/shaky/ShakyService.java
@@ -2,6 +2,7 @@ package br.com.claudiobs07.simpleretry.shaky;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,17 @@ public class ShakyService {
     private static final Logger logger = LoggerFactory.getLogger(ShakyService.class);
 
     @Recover
-    public int fallback(RuntimeException ex) {
+    public int fallback(BoomException ex) {
+        logger.info("calling fallback");
         return 2;
     }
 
-    @Retryable(include = BoomException.class)
+    @Retryable(include = BoomException.class, maxAttempts = 1, backoff = @Backoff(delay = 5000))
     public int deriveNumber() throws Exception {
         logger.info("calling derive number");
         if (Math.random() > 0.5) {
             Thread.sleep(1000 * 3);
-            throw new RuntimeException("Boom!");
+            throw new BoomException("Boom!");
         }
         return 1;
     }
