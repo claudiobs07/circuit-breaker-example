a105f77e34966ce0e8e936f3c36d9c41e23844ac
Claudio Santos <claudiobs07@gmail.com>
2018-04-16 16:09:22 -0300

    add: hystrix properties

--- simple/src/main/java/br/com/claudiobs07/simple/shaky/ShakyService.java
+++ simple/src/main/java/br/com/claudiobs07/simple/shaky/ShakyService.java
@@ -1,19 +1,26 @@
 package br.com.claudiobs07.simple.shaky;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShakyService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ShakyService.class);
+
     public int fallback() {
         return 2;
     }
 
-    @HystrixCommand(fallbackMethod = "fallback")
+    @HystrixCommand(commandKey = "deriveNumber", fallbackMethod = "fallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "30000")
+    })
     public int deriveNumber() throws Exception {
+        Thread.sleep(1000 * 3);
         if (Math.random() > 0.5) {
-            Thread.sleep(1000 * 3);
             throw new RuntimeException("Boom!");
         }
         return 1;
